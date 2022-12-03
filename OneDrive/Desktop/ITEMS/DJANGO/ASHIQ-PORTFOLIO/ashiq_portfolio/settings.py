0637c10c7d9055a6911ef92aa8b3f25c5b37336c
muzammil-85 <muzukondeth85@gmail.com>
2022-12-03 10:27:41 +0530

    setted

--- OneDrive/Desktop/ITEMS/DJANGO/ASHIQ-PORTFOLIO/ashiq_portfolio/settings.py
+++ OneDrive/Desktop/ITEMS/DJANGO/ASHIQ-PORTFOLIO/ashiq_portfolio/settings.py
@@ -187,20 +187,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# AWS
-
-# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
-
-# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
-# AWS_ACCESS_KEY_ID = 'AKIA6DXLVGOLIMONDIOF'
-
-# AWS_SECRET_ACCESS_KEY = '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'
-
-# AWS_STORAGE_BUCKET_NAME = 'ashiq-portfolio'
-
-# AWS_QUERYSTRING_AUTH = False
-
-# AWS_S3_FILE_OVERWRITE = False
-
-# AWS_DEFAULT_ACL = 'None'
